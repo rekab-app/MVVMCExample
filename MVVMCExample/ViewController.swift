6ead538aaa6030599b9e29dfc18c4ebfe1b1e002
Mehdi Sohrabi <mehdok@gmail.com>
2018-12-08 15:32:47 +0330

    Initial Commit

---
+++ MVVMCExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMCExample
+//
+//  Created by Mehdi Sohrabi on 12/8/18.
+//  Copyright © 2018 sixthsolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
